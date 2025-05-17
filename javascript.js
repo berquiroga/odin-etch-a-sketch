4cf63c88cd3e42ac9ad01e46f8e77a541651e8ee
Bernardo Quiroga <156685978+berquiroga@users.noreply.github.com>
2025-05-17 10:43:41 -0400

    Add hover effect over grid via js, changing color

--- javascript.js
+++ javascript.js
@@ -2,9 +2,15 @@
 const grid = document.querySelector(".grid")
 
 // Generate a 16x16 (256) squares and append to grid
-for (let i = 1; i <=256; i++) {
+for (let i = 1; i <= 256; i++) {
     const square = document.createElement("div");
     square.setAttribute("class", "square");
+    square.style.border = "solid black";
+    square.style.backgroundColor = "white";
+    // Add event listeners for hovering and change color leaving a trail
+    square.addEventListener("mouseover", function (e) {
+        e.target.style.background = "red";
+    });
     grid.appendChild(square);
 };
 

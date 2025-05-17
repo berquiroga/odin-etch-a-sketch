53fb8bdeac7344ff690395c152ad5d46a33d9baa
Bernardo Quiroga <156685978+berquiroga@users.noreply.github.com>
2025-05-17 18:10:57 -0400

    Add remove, create grid functions and reset btn

--- javascript.js
+++ javascript.js
@@ -1,19 +1,64 @@
 // Create the square element and select the grid from the html
-const grid = document.querySelector(".grid")
+const grid = document.querySelector(".grid");
 
-// Generate a 16x16 (256) squares and append to grid
-for (let i = 1; i <= 256; i++) {
-    const square = document.createElement("div");
-    square.setAttribute("class", "square");
-    square.style.border = "solid black";
-    square.style.backgroundColor = "white";
-    // Add event listeners for hovering and change color leaving a trail
-    square.addEventListener("mouseover", function (e) {
-        e.target.style.background = "red";
-    });
-    grid.appendChild(square);
+// Function to get the size of the grid (max 100 squares per side)
+function getSize() {
+    while (true) {
+        var size = prompt("How many squares you want per side on the new grid?");
+        if (size <= 100 && size > 0) {
+            return size;
+        } else {
+            alert("The minimum amount of squares per side is 1 and the maximum is 100!");
+            continue;
+        };
+    };
 };
 
-console.log(grid);
+// Function for creating grid according to size given
+function createGrid(side) {
+    for (let i = 1; i <= side * side; i++) {
+        // Create square div and add style via js
+        const square = document.createElement("div");
+        square.setAttribute("class", "square");
+        square.style.border = "solid black";
+        square.style.backgroundColor = "white";
+        square.style.aspectRatio = "1 / 1";
+        let width = 100 / side;
+        square.style.width = `${width}%`;
+        square.style.boxSizing = "border-box";
+        // Randomize square color in RGB (0-255 for each)
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+        // Add event listeners for hovering and change color leaving a trail
+        square.addEventListener("mouseover", function (e) {
+            e.target.style.background = `rgb(${r}, ${g}, ${b})`;
+        });
+        grid.appendChild(square);
+    };
+};
+
+// Function to delete the previous grid before rendering a new one
+function deleteGrid() {
+    grid.replaceChildren();
+};
+
+// Generate a button that prompts the user for the size of the grid and deletes
+// the previous one, generating a new one the size the user specified.
+const body = document.querySelector("body");
+const header = document.createElement("div");
+header.style.display = "flex";
+header.style.justifyContent = "center";
+header.style.padding = "20px";
+body.insertBefore(header, grid);
+
+const resetBtn = document.createElement("button");
+resetBtn.textContent = "Reset grid"
+resetBtn.addEventListener("click", () => {
+    side = getSize();
+    deleteGrid();
+    createGrid(side);
+});
+header.appendChild(resetBtn);
 
 
